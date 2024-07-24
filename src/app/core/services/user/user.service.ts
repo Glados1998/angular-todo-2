246e8d16574245c2bb44c4132fde445fd748aa84
jerome greder <j.greder@staro.ch>
2024-07-24 15:57:03 +0200

    documentation added to user-service

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -14,10 +14,22 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Registers a new user.
+   *
+   * @param user - The user object containing registration details.
+   * @returns An Observable of the registered user.
+   */
   register(user: User) {
     return this.http.post<User>(`${this.apiUrl}/register`, user);
   }
 
+  /**
+   * Logs in a user.
+   *
+   * @param user - An object containing the user's email and password.
+   * @returns An Observable of the user login response.
+   */
   login(user: { email: string, password: string }) {
     return this.http.post<UserLogin>(`${this.apiUrl}/login`, user).pipe(
       tap((response: any) => {
@@ -28,24 +40,50 @@ export class UserService {
     );
   }
 
+  /**
+   * Logs out the current user.
+   */
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     this.loginStatusSubject.next(false);
   }
 
+  /**
+   * Deletes a user account.
+   *
+   * @param id - The ID of the user to be deleted.
+   * @returns An Observable of the delete operation.
+   */
   deleteAccount(id: string) {
     return this.http.delete(`${this.apiUrl}/delete/${id}`);
   }
 
+  /**
+   * Updates account details of a user.
+   *
+   * @param data - An object containing the user ID and the data to be updated.
+   * @returns An Observable of the update operation.
+   */
   updateAccountDetails(data: { id: string, data: any }) {
     return this.http.post(`${this.apiUrl}/update/account-details`, data);
   }
 
+  /**
+   * Updates the password of a user.
+   *
+   * @param data - An object containing the user ID and the new password.
+   * @returns An Observable of the update operation.
+   */
   updatePassword(data: { id: string, password: string}) {
     return this.http.post(`${this.apiUrl}/update/password`, data);
   }
 
+  /**
+   * Retrieves the current login information from local storage.
+   *
+   * @returns An object containing the user and token if logged in, otherwise null.
+   */
   getLogin() {
     const userStr = localStorage.getItem('user');
     const token = localStorage.getItem('token');
@@ -55,6 +93,11 @@ export class UserService {
     return null;
   }
 
+  /**
+   * Checks if a user is currently logged in.
+   *
+   * @returns A boolean indicating the login status.
+   */
   private isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
